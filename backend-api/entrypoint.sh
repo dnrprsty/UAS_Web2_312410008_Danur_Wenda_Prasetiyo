c494736779eff75ee8182efc1c590508c9bd69e3
dnrprsty <danurwp70@gmail.com>
2026-06-22 21:37:22 +0700

    update

--- backend-api/entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Jalankan migrasi
-php /var/www/html/spark migrate --force
-
-# Jalankan Apache
-apache2-foreground
--- backend-api/start.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# Railway start script
-cd backend-api
-php spark serve --host=0.0.0.0 --port=${PORT:-8080}
